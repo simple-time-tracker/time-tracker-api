9fb8cff97fbf4f11401553110a7733d776564cd7
Dovydas Venckus <dovydas.venckus@live.com>
2016-04-07 23:44:54 +0300

    Added spring boot

---
+++ src/test/java/com/dovydasvenckus/timetracker/TimeTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dovydasvenckus.timetracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TimeTrackerApplication.class)
+public class TimeTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
