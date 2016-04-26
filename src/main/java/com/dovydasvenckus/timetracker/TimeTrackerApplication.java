45c883f3985c98ffe30c9af9be8dab6ab51f116a
Dovydas Venckus <dovydas.venckus@live.com>
2016-04-26 20:42:19 +0300

    Fixed to persist localdate time as database type instead of byte array

--- src/main/java/com/dovydasvenckus/timetracker/TimeTrackerApplication.java
+++ src/main/java/com/dovydasvenckus/timetracker/TimeTrackerApplication.java
@@ -2,6 +2,12 @@ package com.dovydasvenckus.timetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+@EntityScan(
+		basePackageClasses = { TimeTrackerApplication.class, Jsr310JpaConverters.class }
+)
 
 @SpringBootApplication
 public class TimeTrackerApplication {
