e771c90de3fb2747f20a744b270aba8922c5c2a9
Dovydas Venckus <dovydas.venckus@live.com>
2016-06-13 23:34:16 +0300

    Allowing cors from host in properties

--- api/src/main/java/com/dovydasvenckus/timetracker/config/CorsFilter.java
+++ api/src/main/java/com/dovydasvenckus/timetracker/config/CorsFilter.java
@@ -1,5 +1,7 @@
 package com.dovydasvenckus.timetracker.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -12,16 +14,24 @@ import java.io.IOException;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 class CorsFilter implements Filter {
 
+    private final String allowCorsPattern;
+
+    @Autowired
+    CorsFilter(@Value("${cors.allow:'*'}") String allowPattern) {
+        this.allowCorsPattern = allowPattern;
+    }
+
     @Override
-    public void init(FilterConfig arg0) throws ServletException {}
+    public void init(FilterConfig arg0) throws ServletException {
+    }
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp,
                          FilterChain chain) throws IOException, ServletException {
 
-        HttpServletResponse response=(HttpServletResponse) resp;
+        HttpServletResponse response = (HttpServletResponse) resp;
 
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", allowCorsPattern);
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -30,6 +40,7 @@ class CorsFilter implements Filter {
     }
 
     @Override
-    public void destroy() {}
+    public void destroy() {
+    }
 
 }
