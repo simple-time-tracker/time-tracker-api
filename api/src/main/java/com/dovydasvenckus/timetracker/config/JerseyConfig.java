ea5a19b473d31afca95dde6a14161739858595bf
Dovydas Venckus <dovydas.venckus@live.com>
2016-06-13 23:40:42 +0300

    Added GZIP ing for REST

--- api/src/main/java/com/dovydasvenckus/timetracker/config/JerseyConfig.java
+++ api/src/main/java/com/dovydasvenckus/timetracker/config/JerseyConfig.java
@@ -2,7 +2,9 @@ package com.dovydasvenckus.timetracker.config;
 
 import com.dovydasvenckus.timetracker.entry.TimeEntryController;
 import com.dovydasvenckus.timetracker.project.ProjectController;
+import org.glassfish.jersey.message.GZipEncoder;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.filter.EncodingFilter;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,5 +13,6 @@ public class JerseyConfig extends ResourceConfig {
         register(ObjectMapperContextResolver.class);
         register(ProjectController.class);
         register(TimeEntryController.class);
+        EncodingFilter.enableFor(this, GZipEncoder.class);
     }
 }
