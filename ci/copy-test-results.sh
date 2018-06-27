e39d449ce011076037b4cd42d54298368d210daf
Dovydas Venckus <dovydas.venck@gmail.com>
2018-06-28 00:11:59 +0300

    Fix CI test copy script

--- ci/copy-test-results.sh
+++ ci/copy-test-results.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd $SHIPPABLE_BUILD_DIR
 mkdir -p shippable/testresults
-cp api/build/test-results/test/*.xml shippable/testresults
+cp build/test-results/test/*.xml shippable/testresults
