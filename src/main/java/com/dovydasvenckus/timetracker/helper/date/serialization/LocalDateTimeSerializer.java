ea114e2c8f3e0fa710616cc34c1070c696996c86
Dovydas Venckus <dovydas.venck@gmail.com>
2018-10-20 22:20:41 +0300

    Use ISO date format with time zone for LocalDateTime type

---
+++ src/main/java/com/dovydasvenckus/timetracker/helper/date/serialization/LocalDateTimeSerializer.java
@@ -0,0 +1,20 @@
+package com.dovydasvenckus.timetracker.helper.date.serialization;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeSerializer extends JsonSerializer<LocalDateTime> {
+    @Override
+    public void serialize(LocalDateTime dateTime, JsonGenerator jg,
+                          SerializerProvider sp) throws IOException {
+        Instant instant = dateTime.atZone(ZoneOffset.UTC).toInstant();
+        jg.writeString(DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(instant) + "Z");
+    }
+}
