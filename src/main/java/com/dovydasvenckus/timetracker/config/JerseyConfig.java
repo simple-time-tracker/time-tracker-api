3175606ec04968859be7993c2938df16938af570
Dovydas Venckus <dovydas.venck@gmail.com>
2019-12-01 18:58:32 +0200

    Write context provider for current user

--- src/main/java/com/dovydasvenckus/timetracker/config/JerseyConfig.java
+++ src/main/java/com/dovydasvenckus/timetracker/config/JerseyConfig.java
@@ -1,7 +1,10 @@
 package com.dovydasvenckus.timetracker.config;
 
 import com.dovydasvenckus.timetracker.entry.TimeEntryController;
+import com.dovydasvenckus.timetracker.helper.security.ClientDetails;
+import com.dovydasvenckus.timetracker.helper.security.CurrentUserResolver;
 import com.dovydasvenckus.timetracker.project.ProjectController;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
 import org.glassfish.jersey.message.GZipEncoder;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.filter.EncodingFilter;
@@ -15,7 +18,17 @@ public class JerseyConfig extends ResourceConfig {
         register(ObjectMapperContextResolver.class);
         register(ProjectController.class);
         register(TimeEntryController.class);
+        register(currentUserContext());
         EncodingFilter.enableFor(this, GZipEncoder.class);
         setProperties(Collections.singletonMap("jersey.config.server.response.setStatusOverSendError", true));
+    }
+
+    private AbstractBinder currentUserContext() {
+        return new AbstractBinder() {
+            @Override
+            protected void configure() {
+                bindFactory(CurrentUserResolver.class).to(ClientDetails.class);
+            }
+        };
     }
 }
